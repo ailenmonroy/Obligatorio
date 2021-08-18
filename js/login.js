c7ed95aa8328517bd08ad3bbd89e327536615e35
ailenmonroy <88326506+ailenmonroy@users.noreply.github.com>
2021-08-18 18:15:21 -0300

    Google

--- js/login.js
+++ js/login.js
@@ -33,8 +33,8 @@ function cargarErroresLog(id,idmen) {
 function redirigir(){
     var usuario = document.getElementById('user');
     var contra = document.getElementById('password');
+    var e = document.getElementById('err');
     if(usuario.value == ''||contra.value == '' ){
-        var e = document.getElementById('err');
         e.style.display = "block";
     }else{
         e.style.display = "none";
@@ -42,10 +42,20 @@ function redirigir(){
     }
 }
 
+
+
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+ 
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+
