56cfa33c3a3ca126e229f575ff02d1444c6beaf9
ailenmonroy <88326506+ailenmonroy@users.noreply.github.com>
2021-08-18 17:15:30 -0300

    actualizacion

--- js/login.js
+++ js/login.js
@@ -4,3 +4,48 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+
+
+function cargarErroresLog(id,idmen) {
+    var elem = document.getElementById(id);
+    var e = document.getElementById(idmen);
+   
+    if(elem.value == ''){
+
+        if(e.id == 'us'){
+            e.innerHTML = "Username required."
+            
+        }else{
+            e.innerHTML = "Password required.";
+        }
+        e.style.display = "block";
+        elem.classList.add("error");    
+    
+        
+    }else{
+        e.style.display = "none";
+        elem.classList.remove("error");
+    }
+}
+
+
+function redirigir(){
+    var usuario = document.getElementById('user');
+    var contra = document.getElementById('password');
+    if(usuario.value == ''||contra.value == '' ){
+        var e = document.getElementById('err');
+        e.style.display = "block";
+    }else{
+        e.style.display = "none";
+        location.replace("file:///C:/Users/Ailen/OneDrive/Desktop/Obligatorio/Workspace%20inicial/index.html");
+    }
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
--- js/products.js
+++ js/products.js
@@ -1,6 +1,51 @@
+var prodArray = [];
+
+function showProdList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let prod = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + prod.imgSrc + `" alt="` + prod.name + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ prod.name +`</h4>
+                        <small class="text-muted">` + prod.soldCount + ` artículos</small>
+                        
+                    </div>
+                    <p class="text-muted">` + prod.description + ` artículos</p>
+                    <p class="text-muted">` + prod.cost +  prod.currency +` </p>
+                   
+                    
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("list").innerHTML = htmlContentToAppend;
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function () {
+    showSpinner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            prodArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProdList(prodArray);
+        }
+    });
 
 });
+
