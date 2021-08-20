c9b872cd6c6774bdc1eedb3a928ad71516b63eff
ailenmonroy <88326506+ailenmonroy@users.noreply.github.com>
2021-08-20 18:18:53 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -51,26 +51,26 @@ function redirigir(){
 
 function onSignIn(googleUser) {     
     var profile = googleUser.getBasicProfile();     
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.     
+    console.log('ID: ' + profile.getId());     
     console.log('Name: ' + profile.getName());     
     console.log('Image URL: ' + profile.getImageUrl());     
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.     
+    console.log('Email: ' + profile.getEmail()); 
     var id_token = googleUser.getAuthResponse().id_token;     
     console.log(id_token);     
-    localStorage.setItem("usuario", profile.getEmail()); //setItem almacena el dato en la posición "usuario"     
-    sessionStorage.setItem("usuario", profile.getEmail()); 
+    localStorage.setItem('uses', profile.getEmail());   
+    sessionStorage.setItem('user', profile.getEmail()); 
     sessionStorage.setItem('log',true);
 };  
 
-    function signOut() {     
-        var auth2 = gapi.auth2.getAuthInstance();     
-        localStorage.clear();     
-        sessionStorage.clear();     
-        auth2.signOut().then(function() {         
-            console.log('User signed out.');         
-            location.href = "login.html";      
-        }); 
-    };
+function signOut() {     
+    var auth2 = gapi.auth2.getAuthInstance();     
+    localStorage.clear();     
+    sessionStorage.clear();     
+    auth2.signOut().then(function() {         
+        console.log('User signed out.');         
+        location.href = "login.html";      
+    }); 
+};
 
 
 
