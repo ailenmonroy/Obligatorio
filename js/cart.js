e7c91419122e2f85d190021c389275ed202ff261
ailenmonroy <88326506+ailenmonroy@users.noreply.github.com>
2021-11-10 11:16:21 -0300

    entrega7

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,8 @@ var total = 0;
 
 
 function costEnvTot(){
+    var costo = document.getElementById("costoEn");
+    var cost = document.getElementById("costoT");
     var c = document.getElementById("canti").value;
     let sub = cartInfoD.articles[0].unitCost*c;
     let total = sub + (sub*22)/100;
@@ -15,6 +17,8 @@ function costEnvTot(){
     if(tipo == ""){
         costoE.style.display = "none";
         costoT.style.display = "none";
+        costo.style.display = "none";
+        cost.style.display = "none";
     }else{
         if(tipo == "prem"){
             envio = (sub*15)/100; 
@@ -31,6 +35,8 @@ function costEnvTot(){
         costoT.innerHTML = ct + " " + cartInfoD.articles[0].currency;
         costoE.style.display = "block";
         costoT.style.display = "block";
+        costo.style.display = "block";
+        cost.style.display = "block";
     }
 
     
@@ -90,53 +96,134 @@ function Calcularsub(){
     costEnvTot();
 }
 
+function error(id,idMen){
+    var campo = document.getElementById(id);
+    var error = document.getElementById(idMen);
 
-function errorPago(){
-    var m = document.getElementById("metodo").value;
-    var nombre = document.getElementById("nomC");
-    var dni = document.getElementById("dni");
-    var numt = document.getElementById("numT");
-    var nums = document.getElementById("numS");
-    var fe = document.getElementById("fec");
-    var numc = document.getElementById("numC");
-    var co = document.getElementById("cont");
+    if(campo.value == ""){
+        campo.classList.add("error");
+        error.style.display = "block";
+    }else{
+        campo.classList.remove("error");
+        error.style.display = "none";    
+    }
+}
 
-    if(m !== ""){
-        if(m =="cred"&&(((((nombre.value==""||dni.value=="")||numt.value=="")||nums.value=="")||fe.value==""))){
+
+function errorPago(mod){
+    if(mod == 'credito'){
+        var nombre = document.getElementById("nomC");
+        var dni = document.getElementById("dni");
+        var numt = document.getElementById("numT");
+        var nums = document.getElementById("numS");
+        var fe = document.getElementById("fec");
+        var er = document.getElementById("e1");
+        if(((((nombre.value==""||dni.value=="")||numt.value=="")||nums.value=="")||fe.value=="")){
             nombre.classList.add("error");
             dni.classList.add("error");
             numt.classList.add("error");
             nums.classList.add("error");
             fe.classList.add("error");
-            numc.classList.remove("error");
-            co.classList.remove("error");
-        }else if(m =="transf"&&(nombre.value==""||dni.value==""||numc.value==""||co.value=="")){
-            nombre.classList.add("error");
-            dni.classList.add("error");
-            numt.classList.remove("error");
-            nums.classList.remove("error");
-            fe.classList.remove("error");
-            numc.classList.add("error");
-            co.classList.add("error");
+            er.style.display = "block";
         }else{
             nombre.classList.remove("error");
             dni.classList.remove("error");
             numt.classList.remove("error");
             nums.classList.remove("error");
             fe.classList.remove("error");
-            numc.classList.remove("error");
-            co.classList.remove("error");
+            er.style.display = "none";
         }
-    }else{
-        nombre.classList.remove("error");
-        dni.classList.remove("error");
-        numt.classList.remove("error");
-        nums.classList.remove("error");
-        fe.classList.remove("error");
-        numc.classList.remove("error");
-        co.classList.remove("error");
+
+
+
+
+
+    }else if(mod == 'banco'){//banco
+        var nomb = document.getElementById("nomB");
+        var dn = document.getElementById("dnib");
+        var numc = document.getElementById("numC");
+        var co = document.getElementById("cont");
+        var err = document.getElementById("e2");
+        if(((((nomb.value==""||dn.value=="")||numt.value=="")||nums.value=="")||fe.value=="")){
+            nomb.classList.add("error");
+            dn.classList.add("error");
+            numc.classList.add("error");
+            co.classList.add("error");
+            err.style.display = "block";
+        }else{
+            nomb.classList.remove("error");
+            dn.classList.remove("error");
+            numc.classList.add("error");
+            co.classList.add("error");
+            err.style.display = "none";
+        }
+
+
     }
 }
+
+function abrirModal(){
+    var v = document.getElementById("metodo");
+    if(v.value == "cred"){
+        $("#credito").modal();
+    }else if(v.value == "transf"){
+        $("#banco").modal();
+    }
+}
+
+var ex = false;
+var mensaje = "";
+
+getJSONData(CART_BUY_URL).then(function(result){
+    if (result.status === "ok"){
+        mensaje = result.data.msg;
+    }
+}); 
+
+
+function mostrarExito(){
+    var d = document.getElementById("exito");
+
+    if(ex){
+        d.innerHTML = mensaje;
+        $("#exitoModal").modal();
+    }else{
+        d.innerHTML = "";
+    }
+}
+
+
+function errorCompra(){
+    var nom = document.getElementById("nom");
+    var pais = document.getElementById("pais");
+    var dir = document.getElementById("direccion");
+    var tel = document.getElementById("contact");
+    var env = document.getElementById("tipoEnv");
+    var pago = document.getElementById("metodo");
+    var e = document.getElementById("errorG");
+    if(((((nom.value==""||pais.value=="")||dir.value=="")||tel.value=="")||env.value=="")||pago.value ==""){
+        nom.classList.add("error");
+        pais.classList.add("error");
+        dir.classList.add("error");
+        tel.classList.add("error");
+        env.classList.add("error");
+        pago.classList.add("error");
+        e.style.display = "block";
+        ex = false;
+    }else{
+        nom.classList.remove("error");
+        pais.classList.remove("error");
+        dir.classList.remove("error");
+        tel.classList.remove("error");
+        env.classList.remove("error");
+        pago.classList.remove("error");
+        e.style.display = "none";
+        ex = true;
+    }
+    mostrarExito();
+}
+
+
 
 
 
